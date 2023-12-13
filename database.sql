f21c3545cd7d95bb62d0e2888aeb1afcc28f0779
Romain VANEL <romainvanel@hotmail.com>
2023-12-13 16:10:32 +0100

    modif database.sql

--- database.sql
+++ database.sql
@@ -1,42 +1,46 @@
 CREATE DATABASE IF NOT EXISTS blog_db;
 USE blog_db;
 
+-- Users
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100),
-    email VARCHAR(100),
-    password VARCHAR(100)
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS comments (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
-    content TEXT,
-    comment_date DATETIME,
-    user_id INT NOT NULL,
-    article_id INT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS articles (
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(255),
-    content TEXT,
-    cover VARCHAR(255),
-    publication_date DATETIME,
-    user_id INT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS articles_categories (
-    article_id INT NOT NULL,
-    category_id INT NOT NULL
-);
-
+-- Categories
 CREATE TABLE IF NOT EXISTS categories (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(100)
+    name VARCHAR(100) NOT NULL
 );
 
-ALTER TABLE comments ADD CONSTRAINT fk_comments_user_id FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE comments ADD CONSTRAINT fk_comments_article_id FOREIGN KEY (article_id) REFERENCES articles(id);
-ALTER TABLE articles ADD CONSTRAINT fk_articles_user_id FOREIGN KEY (user_id) REFERENCES users(id);
-ALTER TABLE articles_categories ADD CONSTRAINT fk_articles_categories_article_id FOREIGN KEY (article_id) REFERENCES articles(id);
-ALTER TABLE articles_categories ADD CONSTRAINT fk_articles_categories_category_id FOREIGN KEY (category_id) REFERENCES categories(id);
+-- Articles
+CREATE TABLE IF NOT EXISTS articles (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    content TEXT NOT NULL,
+    cover VARCHAR(255),
+    publication_date DATETIME NOT NULL,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+-- Comments
+CREATE TABLE IF NOT EXISTS comments (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    content TEXT NOT NULL,
+    comment_date DATETIME NOT NULL,
+    user_id INT NOT NULL,
+    article_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (article_id) REFERENCES articles(id)
+);
+
+-- Articles & Categories
+CREATE TABLE IF NOT EXISTS articles_categories (
+    article_id INT NOT NULL,
+    category_id INT NOT NULL,
+    FOREIGN KEY (article_id) REFERENCES articles(id),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
